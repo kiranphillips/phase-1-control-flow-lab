88a315d17007838ed6e2164adef0580a227866df
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-16 21:55:47 -0600

    commit tree b42cd57d3315589db7973bc2c3e48f1e6a92030c

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(amount) {
+  let message
+  if (amount <= 400){
+    message = "This one is on me!"
+  }
+  else if (amount > 400 && amount < 2000){
+    message = "That will be twenty bucks."
+  } 
+  else if (amount > 2000 && amount < 2500){
+  message = "I will gladly take your thirty bucks."}
+  else{
+     message = 'No can do.'}
+
+     return message
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  const response = (city === 'NYC' ? "Ok, sounds good." : "No go.")
+  return response
+  
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(gratuity){
+
+
+  switch(gratuity) {
+      case 'generous':
+         return  "Thank you so much."
+   
+      case 'not as generous':
+        return "Thank you."
+  default:
+   
+     return "Bye."
   // Write your code here!
-}
+}}
